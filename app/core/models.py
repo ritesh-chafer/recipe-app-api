582e8d21c2bdc79f28cbb2f7872ca6f0fb6b03ed
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-02-06 14:58:04 +0530

    created user model

--- app/core/models.py
+++ app/core/models.py
@@ -6,14 +6,14 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
         user = self.model(email=email, **extra_fields)
-        user.set_paswword(password)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
-    email = models.EmailField(max_lemgth=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
