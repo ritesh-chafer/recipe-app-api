47db5e62340d905c2636e5ffdb734f78d45b3c8c
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-02-06 14:54:38 +0530

    user_model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and saves a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_paswword(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_lemgth=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,12 +4,13 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an eamil is successful"""
+        username = "test"
         email = "test@test.com"
         password = "testpass123"
         user = get_user_model().objects.create_user(
+            username = username,
             email = email,
             password = password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
