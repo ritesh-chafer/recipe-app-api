807cc992d6304aafb6956542b308e723eaace523
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-02-13 15:04:52 +0530

    user app

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,9 +7,48 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
-def create_user_model(**params):
+def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
     
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email' : "test@test.com",
+            'password' : "testpass123",
+            'name' : "test"
+        }
+        res = self.cliet.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        payload = {
+            'email' : "test@test.com",
+            'password' : "testpass123",
+            'name' : "test"
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_password__too_short(self):
+        """Test that the password must be more tha 5 characters"""
+        payload = {'email': 'test@test.com', 'password':'test'}
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists =get_user_model().objects.filter(
+            email = payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
