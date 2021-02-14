66bb12bed6eb51d18306a7a17caabcdd5263b470
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-02-14 15:57:14 +0530

    Auth

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -5,25 +5,28 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
+
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
+
 class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
-    
+
     def setUp(self):
         self.client = APIClient()
 
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
         payload = {
-            'email' : "test@test.com",
-            'password' : "testpass123",
-            'name' : "test"
+            'email': 'test@test.com',
+            'password': 'testpass123',
+            'name': 'test'
         }
-        res = self.cliet.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
@@ -31,24 +34,61 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
+        """Test creatinga  user that already exists fails"""
         payload = {
-            'email' : "test@test.com",
-            'password' : "testpass123",
-            'name' : "test"
+            'email': 'test@test.com',
+            'password': 'testpass123',
+            'name': 'test',
         }
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
-    def test_password__too_short(self):
-        """Test that the password must be more tha 5 characters"""
-        payload = {'email': 'test@test.com', 'password':'test'}
+
+    def test_password_too_short(self):
+        """Test that the password must be more than 5 characters"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'pw',
+            'name': 'test',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists =get_user_model().objects.filter(
-            email = payload['email']
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {'email': 'test@test.com', 'password': 'testpass123'}
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        create_user(email='test@test.com', password="testpass123")
+        payload = {'email': 'test@test.com', 'password': 'wrong'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user doesn't exist"""
+        payload = {'email': 'test@test.com', 'password': 'testpass123'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that email and password are required"""
+        res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
