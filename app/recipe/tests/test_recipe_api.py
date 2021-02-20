02fc0b8c9a4c69de4ac2831cb9279f6e92f0f78c
ritesh-chafer <riteshprasadsingh8@gmail.com>
2021-02-21 02:40:27 +0530

    serializers_recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,14 +9,13 @@ from core.models import Recipe, Tag, Ingredient
 
 from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
-RECIPES_URL = reverse("recipe:Recipe-list")
-
+RECIPES_URL = reverse("recipe:recipe-list")
 
 def sample_recipe(user, **params):
     """create and return a sample recipe"""
     defaults={
-        'title' : 'Streak and mushroom sauce',
-        'time_minutes':5,
+        'title' : 'Sample recipe ',
+        'time_minutes': 10,
         'price' : 5.00
     }
 
@@ -33,5 +32,46 @@ class PublicRecipeApiTests(TestCase):
     def test_login_required(self):
         """Test that login is required to access the endpoint"""
         res = self.client.get(RECIPES_URL)
- 
+
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateRecipeApiTest(TestCase):
+    """Test unauthenticated recipe API access"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@test.com',
+            'testpass123'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_recipes(self):
+        """Test retrieving a list of recipes"""
+        sample_recipe(user=self.user)
+        sample_recipe(user=self.user)
+
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_recipes_limited_to_user(self):
+        """Test retrieving recipes for user"""
+        user2 = get_user_model().objects.create_user(
+            'test1@test.com',
+            'testpass1234'
+        )
+        sample_recipe(user=user2)
+        sample_recipe(user=self.user)
+
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.filter(user=self.user)
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -57,3 +57,11 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     # def perform_create(self, serializer):
     #     """Create a new tag"""
     #     serializer.save(user=self.request.user)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
